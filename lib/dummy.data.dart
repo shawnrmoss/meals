de0e56dc8aa933cb8eb97be802741143d20a3ab3
shawnrmoss <me@shawnrmoss.ca>
2019-11-12 22:07:33 -0600

    Category Screen

---
+++ lib/dummy.data.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import './models/category.model.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick and Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Roman',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Rick Ross',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Don Cherry',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Computers',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Dogs and Cats',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Pillows',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Montreal Smoked',
+    color: Colors.pink,
+  ),
+];
